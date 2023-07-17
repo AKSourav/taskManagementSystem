b42b4ec00dfe6f236b7c9224a00a0baa35f835db
AKSourav <anupam.sony32@gmail.com>
2023-07-17 19:37:13 +0530

    fixing

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ if(process.env.NODE_ENV=='production')
 }
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT,console.log(`Server Started on PORT http://localhost:${PORT}`));
